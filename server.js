b09c08c77b70c7dbda0727c560e1914bc7c03993
gagalName <ramadhaneffendi98@gmail.com>
2025-02-24 04:03:28 +0700

    backend

---
+++ server.js
@@ -0,0 +1,59 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+
+import employeeRoutes from "./routes/employeeRoute.js";
+import unitRoutes from "./routes/unitRoute.js";
+import positionRoutes from "./routes/position.js";
+import authRoutes from "./routes/authRoute.js";
+import dashboardRoutes from "./routes/dashboard.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(
+  cors({
+    origin: "https://ramaffendi.github.io", // Sesuaikan dengan frontend
+    methods: "GET,PUT,POST,DELETE",
+    credentials: true, // Izinkan pengiriman cookie atau header authorization
+  })
+); // ⬅️ Pastikan `withCredentials` bisa dipakai
+app.use(express.json());
+
+// Tambahkan session middleware dengan MongoDB Store
+app.use(
+  session({
+    secret: "supersecretkey",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://ramadhan:12345@cluster0.pbcfv.mongodb.net/rsiadb?retryWrites=true&w=majority", // Ganti dengan URL MongoDB Atlas kamu
+    }),
+    cookie: { secure: true }, // Ubah jadi `true` jika pakai HTTPS
+  })
+);
+
+// Routes
+app.use("/api/employees", employeeRoutes);
+app.use("/api/units", unitRoutes);
+app.use("/api/positions", positionRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://ramadhan:12345@cluster0.pbcfv.mongodb.net/rsiadb?retryWrites=true&w=majority"
+  ) // URL MongoDB Atlas kamu
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
